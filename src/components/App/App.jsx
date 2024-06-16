97e44bc74912e61644f9a4f536ce0660af595b10
Olevihon <charleksvik@gmail.com>
2024-06-16 11:25:29 +0300

    setup project + add components and json

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import Profile from '../Profile/Profile.jsx';
+import FriendList from '../FriendList/FriendList.jsx';
+import TransactionHistory from '../TransactionHistory/TransactionHistory.jsx';
+
+import userData from '../../userData.json';
+import friends from '../../friends.json';
+import transactions from '../../transactions.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import FriendListItem from '../FriendListItem/FriendListItem.jsx';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map(friend => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
